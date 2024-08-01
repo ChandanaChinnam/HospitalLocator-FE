46eb44a1c803c54b977e5ee6e2c98ad95105c5d4
Chandana <chinnamchandana26@gmail.com>
2024-08-01 02:48:21 -0700

    commiting changes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66ab545415a31920735876pel4c8ba1";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-cloud-cloud-user-4.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
